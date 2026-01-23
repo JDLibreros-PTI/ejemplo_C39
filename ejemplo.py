461d9d806d09d91d95ecf498c9b4afd1380991de
John Mera <jjmebur@gmail.com>
2026-01-22 19:45:44 -0500

    commit John

--- ejemplo.py
+++ ejemplo.py
@@ -1,2 +1,10 @@
 print("Hola Mundo")
 print("Esta es una prueba de GIT")
+
+def suma(b,a):
+    resultado = b+a
+    return resultado
+
+def suma(c,d,a):
+    resultado = c+d+a
+    return resultado
